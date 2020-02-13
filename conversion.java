c909bb292fc68f78f637a49242ec8d0b6f84c112
michaelphamct <47987943+michaelphamct@users.noreply.github.com>
2020-02-12 20:46:06 -0500

    Ask for precision level every loop, not just first time
    Sorry about the formatting, not really sure why it all changed

--- conversion.java
+++ conversion.java
@@ -23,133 +23,148 @@
 // Import Java Libraries
 import java.util.*;
 
-public class conversion
-{
+public class conversion {
 
-    public static void main(String[]args){
-        System.out.println("(Not So) On-line Measurement Conversion"); // on first run, print menu
-        Scanner menuInput = new Scanner(System.in); //get user input
+    public static void main(String[] args) {
+        // on first run, print menu
+        System.out.println(
+                            "\n"
+                            + "*****************************************\n"
+                            + "*(Not So) On-line Measurement Conversion*\n"
+                            + "*****************************************\n");
 
-        System.out.println("For your precision, please Enter a number between 1 and 4:");
+        Scanner menuInput = new Scanner(System.in); // get user input
+
+        // The instructions say the accuracy levels are from 0 to 4
+        // 0 would just be an int like 212
+        // 1 would be like 212.6
+        // 2 would produce 212.65 and so on for 3 and 4
+        System.out.println("Please enter a number between 0 and 4 to set the number of digits after the decimal mark:");
         int precision = Integer.parseInt(menuInput.nextLine());
 
         printMenu(); // print menu will ask for number between 1 and 14
         int i = Integer.parseInt(menuInput.nextLine());
 
-        // user should put in a number between 1 and 14, else the while loop is not entered.
-        while ((i > 0) && (i < 15))  {
+        // user should put in a number between 1 and 14, else the while loop is not
+        // entered.
+        while ((i > 0) && (i < 15)) {
             // once an option chosen, ask the user what number they want to convert
             System.out.println("What number would you like to convert?\n");
             String c = menuInput.nextLine();
             float userInput = (Float.valueOf(c).floatValue());
             float retVal = (float) 0.0;
             switch (i) {
-                case 1:
-                    System.out.println("Selected option is Fahrenheit to Celsius");
-                    System.out.print("Conversion result is: ");
-                    retVal = convertF2C(userInput);
-                    break;
-                case 2:
-                    System.out.println("Selected option is Celsius to Fahrenheit");
-                    System.out.print("Conversion result is: ");
-                    retVal = convertC2F(userInput);;
-                    break;
-                case 3:
-                    System.out.println("Selected option is Inches to Centimeters");
-                    System.out.print("Conversion result is: ");
-                    retVal = convertIn2Cm(userInput);
-                    break;
-                case 4:
-                    System.out.println("Selected option is Centimeters to Inches");
-                    System.out.print("Conversion result is: ");
-                    retVal = convertCm2In(userInput);
-                    break;
-                case 5:
-                    System.out.println("Selected option is Feet to Meters");
-                    System.out.print("Conversion result is: ");
-                    retVal = convertF2M(userInput);
-                    break;
-                case 6:
-                    System.out.println("Selected option is Meters to Feet");
-                    System.out.print("Conversion result is: ");
-                    retVal= convertM2F(userInput);
-                    break;
-                case 7:
-                    System.out.println("Selected option is Miles to Kilometers");
-                    System.out.print("Conversion result is: ");
-                    retVal = convertM2K(userInput);
-                    break;
-                case 8:
-                    System.out.println("Selected option is Kilometers to Miles");
-                    System.out.print("Conversion result is: ");
-                    retVal = convertK2M(userInput);
-                    break;
-                case 9:
-                    System.out.println("Selected option is Gallons to Liters");
-                    System.out.print("Conversion result is: ");
-                    retVal = convertG2L(userInput);
-                    break;
-                case 10:
-                    System.out.println("Selected option is Liters to Gallons");
-                    System.out.print("Conversion result is: ");
-                    retVal = convertL2G(userInput);
-                    break;
-                case 11:
-                    System.out.println("Selected option is Ounces to Gallons");;
-                    System.out.print("Conversion result is: ");
-                    retVal = convertOz2G(userInput);
-                    break;
-                case 12:
-                    System.out.println("Selected option is Gallons to Ounces");
-                    System.out.print("Conversion result is: ");
-                    retVal = convertG2Oz(userInput);
-                    break;
-                case 13:
-                    System.out.println("Selected option is Pounds to Kilograms");
-                    System.out.print("Conversion result is: ");
-                    retVal = convertLb2K(userInput);
-                    break;
-                case 14:
-                    System.out.println("Selected option is Kilograms to Pounds");
-                    System.out.print("Conversion result is: ");
-                    retVal = convertK2Lb(userInput);
-                    break;
-                default:
-                    System.out.println("Please enter a valid menu item.");
-                    break;
+            case 1:
+                System.out.println("Selected option is Fahrenheit to Celsius");
+                System.out.print("Conversion result is: ");
+                retVal = convertF2C(userInput);
+                break;
+            case 2:
+                System.out.println("Selected option is Celsius to Fahrenheit");
+                System.out.print("Conversion result is: ");
+                retVal = convertC2F(userInput);
+                break;
+            case 3:
+                System.out.println("Selected option is Inches to Centimeters");
+                System.out.print("Conversion result is: ");
+                retVal = convertIn2Cm(userInput);
+                break;
+            case 4:
+                System.out.println("Selected option is Centimeters to Inches");
+                System.out.print("Conversion result is: ");
+                retVal = convertCm2In(userInput);
+                break;
+            case 5:
+                System.out.println("Selected option is Feet to Meters");
+                System.out.print("Conversion result is: ");
+                retVal = convertF2M(userInput);
+                break;
+            case 6:
+                System.out.println("Selected option is Meters to Feet");
+                System.out.print("Conversion result is: ");
+                retVal = convertM2F(userInput);
+                break;
+            case 7:
+                System.out.println("Selected option is Miles to Kilometers");
+                System.out.print("Conversion result is: ");
+                retVal = convertM2K(userInput);
+                break;
+            case 8:
+                System.out.println("Selected option is Kilometers to Miles");
+                System.out.print("Conversion result is: ");
+                retVal = convertK2M(userInput);
+                break;
+            case 9:
+                System.out.println("Selected option is Gallons to Liters");
+                System.out.print("Conversion result is: ");
+                retVal = convertG2L(userInput);
+                break;
+            case 10:
+                System.out.println("Selected option is Liters to Gallons");
+                System.out.print("Conversion result is: ");
+                retVal = convertL2G(userInput);
+                break;
+            case 11:
+                System.out.println("Selected option is Ounces to Gallons");
+                ;
+                System.out.print("Conversion result is: ");
+                retVal = convertOz2G(userInput);
+                break;
+            case 12:
+                System.out.println("Selected option is Gallons to Ounces");
+                System.out.print("Conversion result is: ");
+                retVal = convertG2Oz(userInput);
+                break;
+            case 13:
+                System.out.println("Selected option is Pounds to Kilograms");
+                System.out.print("Conversion result is: ");
+                retVal = convertLb2K(userInput);
+                break;
+            case 14:
+                System.out.println("Selected option is Kilograms to Pounds");
+                System.out.print("Conversion result is: ");
+                retVal = convertK2Lb(userInput);
+                break;
+            default:
+                System.out.println("Please enter a valid menu item.");
+                break;
             }
             printResult(retVal, precision);
+            // after printing result, ask to choose accuracy level again
+            System.out.println(
+                    "Please enter a number between 0 and 4 to set the number of digits after the decimal mark:");
+            precision = Integer.parseInt(menuInput.nextLine());
+            // print menu again and restart conversion process
             printMenu();
             i = Integer.parseInt(menuInput.nextLine());
         }
         menuInput.close();
     }
 
-    public static void printMenu()
-    {
+    public static void printMenu() {
         System.out.println("Please choose between options 1 to 14 to convert a value:");
         System.out.println(
-                "1. Fahrenheit (Fº)     ----->     Celsius (Cº) \n" +
-                "2. Celsius (Cº)        ----->     Fahrenheit (Fº) \n" +
-                "3. Inch (in)           ----->     Centimeter (cm) \n" +
-                "4. Centimeter (cm)     ----->     Inch (in) \n" +
-                "5. Feet (ft)           ----->     Meter (m) \n" +
-                "6. Meter (m)           ----->     Feet (ft) \n" +
-                "7. Mile (mi)           ----->     Kilometer (km) \n" +
-                "8. Kilometer (km)      ----->     Mile (mi) \n" +
-                "9. Gallon (gal)        ----->     Liter (L) \n" +
-                "10. Liter (L)          ----->     Gallon (gal) \n" +
-                "11. Ounce (oz)         ----->     Gram (g) \n" +
-                "12. Gram (g)           ----->     Ounce (oz) \n" +
-                "13. Pound (lb)         ----->     Kilogram (kg) \n" +
-                "14. Kilogram (kg)      ----->     Pound (lb) \n" +
-                "15. Quit\n");
+                  "1. Fahrenheit (Fº)     ----->     Celsius (Cº)\n"
+                + "2. Celsius (Cº)        ----->     Fahrenheit (Fº)\n"
+                + "3. Inch (in)           ----->     Centimeter (cm)\n"
+                + "4. Centimeter (cm)     ----->     Inch (in)\n"
+                + "5. Feet (ft)           ----->     Meter (m)\n"
+                + "6. Meter (m)           ----->     Feet (ft)\n"
+                + "7. Mile (mi)           ----->     Kilometer (km)\n"
+                + "8. Kilometer (km)      ----->     Mile (mi)\n"
+                + "9. Gallon (gal)        ----->     Liter (L)\n"
+                + "10. Liter (L)          ----->     Gallon (gal)\n"
+                + "11. Ounce (oz)         ----->     Gram (g)\n"
+                + "12. Gram (g)           ----->     Ounce (oz)\n"
+                + "13. Pound (lb)         ----->     Kilogram (kg)\n"
+                + "14. Kilogram (kg)      ----->     Pound (lb)\n"
+                + "15. Quit\n");
 
     }
 
-    // Prints the result of conversion with a level of precision specified by the user
-    public static void printResult(float input, float precision)
-    {
+    // Prints the result of conversion with a level of precision specified by the
+    // user
+    public static void printResult(float input, float precision) {
         if (precision == 4) {
             System.out.println(String.format("%.4f", input));
         } else if (precision == 3) {
@@ -164,90 +179,75 @@ public class conversion
         System.out.println('\n');
     }
 
-/** *****************************************************
- *  Conversion methods below
- *  14 methods, 2 for each pair of units
-********************************************************* */
-    private static float convertF2C (float input)
-    {  // Convert farenheit to celsius
-        return (float) ( ( (input-32.0) * 5.0) / 9.0);
+    /**
+     * ***************************************************** Conversion methods
+     * below 14 methods, 2 for each pair of units
+     */
+    private static float convertF2C(float input) { // Convert farenheit to celsius
+        return (float) (((input - 32.0) * 5.0) / 9.0);
     }
 
-    private static float convertC2F (float input)
-    {  // Convert celsius to farenheit
-        return (float) ( (input * 9.0 / 5.0) + 32.0);
+    private static float convertC2F(float input) { // Convert celsius to farenheit
+        return (float) ((input * 9.0 / 5.0) + 32.0);
     }
 
     // small distance
-    private static float convertIn2Cm (float input)
-    {  // Convert inches to centimeters
+    private static float convertIn2Cm(float input) { // Convert inches to centimeters
         return (float) (input * 2.54);
     }
 
-    private static float convertCm2In (float input)
-    {  // Convert centimeters to inches
+    private static float convertCm2In(float input) { // Convert centimeters to inches
         return (float) (input * 0.3937);
     }
 
     // medium distance
-    private static float convertF2M (float input)
-    {  // Convert feet to meters
+    private static float convertF2M(float input) { // Convert feet to meters
         return (float) (input * 0.3048);
     }
 
-    private static float convertM2F (float input)
-    {  // Convert meters to feet
+    private static float convertM2F(float input) { // Convert meters to feet
         return (float) (input / 0.6048);
     }
 
     // large distance
-    private static float convertM2K (float input)
-    {  // Convert miles to kilometers
-        return(float) (input * 1.609);
+    private static float convertM2K(float input) { // Convert miles to kilometers
+        return (float) (input * 1.609);
     }
 
-    private static float convertK2M (float input)
-    {  // Convert kilometers to miles
+    private static float convertK2M(float input) { // Convert kilometers to miles
         return (float) (input * 0.6214);
     }
 
     // volume
-    private static float convertG2L (float input)
-    {  // Convert gallons to liters
+    private static float convertG2L(float input) { // Convert gallons to liters
         return (float) (input * 3.785);
     }
 
-    private static float convertL2G (float input)
-    {  // Convert liters to gallons
+    private static float convertL2G(float input) { // Convert liters to gallons
         return (float) (input / 3.785);
     }
 
     // small weight
-    private static float convertOz2G (float input)
-    {  // Convert ounces to grams
+    private static float convertOz2G(float input) { // Convert ounces to grams
         return (float) (input * 28.35);
     }
 
-    private static float convertG2Oz (float input)
-    {  // Convert grams to ounces
+    private static float convertG2Oz(float input) { // Convert grams to ounces
         return (float) (input / 28.35);
     }
 
     // medium weight
-    private static float convertLb2K (float input)
-    {  // Convert pounds to kilograms
+    private static float convertLb2K(float input) { // Convert pounds to kilograms
         return (float) (input * 0.4536);
     }
 
-    private static float convertK2Lb (float input)
-    {  // Convert kilograms to pounds
+    private static float convertK2Lb(float input) { // Convert kilograms to pounds
         return (float) (input * 2.205);
     }
 
-    private static float convertMphKmph (String inputAsStr)
-    {
-//        String Mph = "Mph";
-//        String Kmph = "Kmph";
+    private static float convertMphKmph(String inputAsStr) {
+        // String Mph = "Mph";
+        // String Kmph = "Kmph";
         float num1, num2; // temporary variables
         num2 = 1;
         int n;
@@ -255,14 +255,16 @@ public class conversion
         return (num2);
     }
 
-//    private static float directionHandler (String unitA, String unitB)    //method for determining which conversion direction is desired,
-//    {                                                                     //assuming we halve the number of conversion methods and make each do both directions
-//        System.out.println("Please select your desired conversion direction:");
-//        System.out.println("1." + unitA + "to" + unitB);
-//        System.out.println("2." + unitB + "to" + unitA);
-//
-//        Scanner menuInput = new Scanner(System.in); //get user input
-//        int i = Integer.parseInt(menuInput.nextLine());
-//
-//    }
+    // private static float directionHandler (String unitA, String unitB) //method
+    // for determining which conversion direction is desired,
+    // { //assuming we halve the number of conversion methods and make each do both
+    // directions
+    // System.out.println("Please select your desired conversion direction:");
+    // System.out.println("1." + unitA + "to" + unitB);
+    // System.out.println("2." + unitB + "to" + unitA);
+    //
+    // Scanner menuInput = new Scanner(System.in); //get user input
+    // int i = Integer.parseInt(menuInput.nextLine());
+    //
+    // }
 }
